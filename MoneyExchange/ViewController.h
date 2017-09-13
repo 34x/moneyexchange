5558946ef9c53eed9f2272bbe620b2835c8a62a4
34x <maks@34x.me>
2017-09-13 10:21:56 +0200

    Initial commit

---
+++ MoneyExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoneyExchange
+//
+//  Created by Max on 13/09/2017.
+//  Copyright © 2017 34x. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MoneyExchange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoneyExchange
+//
+//  Created by Max on 13/09/2017.
+//  Copyright © 2017 34x. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
