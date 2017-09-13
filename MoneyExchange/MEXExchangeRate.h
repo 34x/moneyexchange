f5ffb932966d2607e68b6081f2d4c0dd5f9ddb61
34x <maks@34x.me>
2017-09-13 22:24:29 +0200

    Exchange rate object with calculations

---
+++ MoneyExchange/MEXExchangeRate.h
@@ -0,0 +1,23 @@
+//
+//  MEXExchangeRate.h
+//  MoneyExchange
+//
+//  Created by Max on 13/09/2017.
+//  Copyright © 2017 34x. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MEXCurrency.h"
+#import "MEXMoney.h"
+
+@interface MEXExchangeRate : NSObject
+@property (nonatomic, readonly) MEXCurrency* numerator;
+@property (nonatomic, readonly) MEXCurrency* denominator;
+@property (nonatomic, readonly) NSNumber* ratio;
+
++ (instancetype) rateWith:(MEXCurrency*)numerator over:(MEXCurrency*)denominator withRatio:(NSNumber*)ratio;
+
+- (MEXMoney*)exchangeForward:(MEXMoney*)amount;
+- (MEXMoney*)exchangeBackward:(MEXMoney*)amount;
+
+@end
---
+++ MoneyExchange/MEXExchangeRate.m
@@ -0,0 +1,39 @@
+//
+//  MEXExchangeRate.m
+//  MoneyExchange
+//
+//  Created by Max on 13/09/2017.
+//  Copyright © 2017 34x. All rights reserved.
+//
+
+#import "MEXExchangeRate.h"
+
+@interface MEXExchangeRate ()
+@property (nonatomic, readwrite) MEXCurrency* numerator;
+@property (nonatomic, readwrite) MEXCurrency* denominator;
+@property (nonatomic, readwrite) NSNumber* ratio;
+@end
+
+@implementation MEXExchangeRate
++ (instancetype) rateWith:(MEXCurrency *)numerator
+                     over:(MEXCurrency *)denominator
+                withRatio:(NSNumber *)ratio {
+    
+    MEXExchangeRate* rate = [MEXExchangeRate new];
+    rate.numerator = numerator;
+    rate.denominator = denominator;
+    rate.ratio = ratio;
+    
+    return rate;
+}
+
+
+- (MEXMoney*)exchangeForward:(MEXMoney *)amount {
+    return [amount multiplyBy:self.ratio];
+}
+
+- (MEXMoney*)exchangeBackward:(MEXMoney *)amount {
+    return [amount divideBy:self.ratio];
+}
+
+@end
